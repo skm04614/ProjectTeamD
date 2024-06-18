b479b2ed040cdae1c79edffb53fe1e32a136eec9
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-06-18 15:00:16 +0900

    feat: implement logic for shell full read & help and their associated unit tests (#14)

--- tests/test_custom_shell.py
+++ tests/test_custom_shell.py
@@ -1,13 +1,23 @@
 import io
-
-from contextlib import redirect_stdout
 from unittest import TestCase, skip
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
+from contextlib import redirect_stdout
 
 from custom_shell.custom_shell import CustomShell
 
 
+def _lba_to_sample_val(lba: int) -> int:
+    return lba << 4
+
+
+def _print_lba_to_sample_val(lba: int) -> None:
+    print(_lba_to_sample_val(lba))
+
+
 class TestCustomShell(TestCase):
+    def setUp(self):
+        self.__cshell = CustomShell()
+
     def test_write(self):
         pass
 
@@ -33,10 +43,26 @@ class TestCustomShell(TestCase):
         pass
 
     def test_help(self):
-        pass
+        expected = ("write(lba, val) - writes a val on lba",
+                    "read(lba)       - reads the val written on lba",
+                    "exit()          - exits program",
+                    "help()          - prints manual to stdout",
+                    "full_write(val) - writes val to all lbas ranging from 0 to 99",
+                    "full_read()     - reads all vals written on each lba ranging from 0 to 99 and prints to stdout")
+        expected = '\n'.join(expected)
+
+        with io.StringIO() as buf, redirect_stdout(buf):
+            self.__cshell.help()
+            result = buf.getvalue().strip()
+            self.assertEqual(expected, result)
 
     def test_full_write(self):
         pass
 
-    def test_full_read(self):
-        pass
+    @patch.object(CustomShell, "read", side_effect=_print_lba_to_sample_val)
+    def test_full_read(self, mk_cshell):
+        with io.StringIO() as buf, redirect_stdout(buf):
+            self.__cshell.full_read()
+            result = buf.getvalue().strip()
+            expected = '\n'.join(str(_lba_to_sample_val(lba)) for lba in range(0, 100))
+            self.assertEqual(expected, result)
