f62710bde0e3d73b56e62ac8ee5426a907a0acf6
jinnie <genie121110@gmail.com>
2024-06-18 15:03:33 +0900

    [feat] Custom Shell write Unittest 수정; 잘못된 TC 제외, write test 코드 수정

--- tests/test_custom_shell.py
+++ tests/test_custom_shell.py
@@ -1,3 +1,4 @@
+import os.path
 from unittest import TestCase, skip
 from unittest.mock import Mock
 
@@ -7,22 +8,26 @@ from custom_shell.custom_shell import CustomShell
 class TestCustomShell(TestCase):
     def setUp(self):
         super().setUp()
+        self.cshell = CustomShell()
 
-    def test_write_mock(self):
-        mk = Mock()
-        mk.write(10, 0x12345678)
+    @skip
+    def test_write(self):
+        self.cshell.write(10, 0x1234ABCD)
+        self.cshell.read(10)
 
-        mk.write.assert_called_once_with(10, 0x12345678)
+        with open(os.path.dirname(__file__) + '\\..\\ssd\\result.txt', 'r') as f:
+            ret = f.read().strip()
+
+        self.assertEqual(0x1234ABCD, int(ret, 6))
 
     @skip
     def test_exception_when_invalid_argument_for_write(self):
-        cshell = CustomShell()
-
-        test_arg = [[-1, 0x12345678], [101, 0x12345678], [10, 0x1234], [10, 0x1234ABCDD], [10, 'abcd'], [None, 0x1234ABCD], [10, None]]
+        test_arg = [[-1, 0x12345678], [101, 0x12345678], [10, 0x1234ABCDD], [10, 'abcd'], [None, 0x1234ABCD],
+                    [10, None]]
 
         for lba, val in test_arg:
             with self.assertRaises(ValueError):
-                cshell.write(lba, val)
+                self.cshell.write(lba, val)
 
     def test_read(self):
         pass
