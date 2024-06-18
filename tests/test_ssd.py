4145d02ffc1c8c73c39d8a520ac9f3a5df15da40
Sunghwaa <53073645+Sunghwaa@users.noreply.github.com>
2024-06-18 16:46:23 +0900

    [feat] SSD read 테스트 세분화하여 추가 (#21)

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -28,7 +28,7 @@ class TestSSD(TestCase):
         expected = 0x76543210
 
         # arrange
-        self.ssd.data[lba] = expected
+        self.ssd._data[lba] = expected
 
         # act
         self.ssd.read(lba)
@@ -38,10 +38,33 @@ class TestSSD(TestCase):
             self.assertEqual(expected, int(f.read(), 16))
 
     @skip
-    def test_read_with_invalid_lba(self):
-        invalid_lbas = [-1, 100, ' ', '', None]
+    def test_read_with_out_of_range_lba(self):
+        out_of_range_lbas = [-1, 100]
 
-        for lba in invalid_lbas:
-            with self.subTest(f'SSD read test with invalid lba: {lba}'):
-                with self.assertRaises(ValueError):
+        for lba in out_of_range_lbas:
+            with self.subTest(f'SSD read test with out of range LBA: {lba}'):
+                try:
                     self.ssd.read(lba)
+                    self.fail("ValueError should have been raised.")
+                except ValueError as err:
+                    self.assertEqual("LBA is out of range [0, 100).", err.args[0])
+
+    @skip
+    def test_read_with_none_integer_lba(self):
+        none_integer_lbas = [' ', '', [], {}]
+
+        for lba in none_integer_lbas:
+            with self.subTest(f'SSD read test with none integer LBA: {lba}'):
+                try:
+                    self.ssd.read(lba)
+                    self.fail("TypeError should have been raised.")
+                except TypeError as err:
+                    self.assertEqual("LBA must be an integer.", err.args[0])
+
+    @skip
+    def test_read_with_none_lba(self):
+        try:
+            self.ssd.read(None)
+            self.fail("ValueError should have been raised.")
+        except ValueError as err:
+            self.assertEqual("LBA is required.", err.args[0])
