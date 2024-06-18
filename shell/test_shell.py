8434c80beeaeff1d7fb577971ec77af80ba0d718
heehoha <dolgyuk@gmail.com>
2024-06-18 12:01:51 +0900

    [test] test_shell, test_read() 작성

--- shell/test_shell.py
+++ shell/test_shell.py
@@ -1,12 +1,17 @@
 from unittest import TestCase
+from unittest.mock import Mock
 
+from shell import Shell
 
 class TestShell(TestCase):
     def test_write(self):
         pass
 
     def test_read(self):
-        pass
+        ssd = Mock()
+        ssd.read.return_value = "0xAABBCCDD"
+        shell = Shell()
+        self.assertEqual(shell.read(), "0xAABBCCDD")
 
     def test_exit(self):
         pass
