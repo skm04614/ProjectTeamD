8961893ce54bafc9f82f6b41cd462b9f78321e40
jinnie <genie121110@gmail.com>
2024-06-18 14:10:59 +0900

    [test] Custom Shell write Unittest 추가

--- tests/test_custom_shell.py
+++ tests/test_custom_shell.py
@@ -1,9 +1,28 @@
-from unittest import TestCase
+from unittest import TestCase, skip
+from unittest.mock import Mock
+
+from custom_shell.custom_shell import CustomShell
 
 
 class TestCustomShell(TestCase):
-    def test_write(self):
-        pass
+    def setUp(self):
+        super().setUp()
+
+    def test_write_mock(self):
+        mk = Mock()
+        mk.write(10, 0x12345678)
+
+        mk.write.assert_called_once_with(10, 0x12345678)
+
+    @skip
+    def test_exception_when_invalid_argument_for_write(self):
+        cshell = CustomShell()
+
+        test_arg = [[-1, 0x12345678], [101, 0x12345678], [10, 0x1234], [10, 0x1234ABCDD], [10, 'abcd']]
+
+        for lba, val in test_arg:
+            with self.assertRaises(ValueError):
+                cshell.write(lba, val)
 
     def test_read(self):
         pass
