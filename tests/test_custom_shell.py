70c167e928210dfae26b6551dee7b57fceae53a1
Kevin <skm04614@gmail.com>
2024-06-18 14:28:13 +0900

    feat: custom shell full read unit test

--- tests/test_custom_shell.py
+++ tests/test_custom_shell.py
@@ -1,7 +1,23 @@
-from unittest import TestCase
+import io
+from unittest import TestCase, skip
+from unittest.mock import patch
+from contextlib import redirect_stdout
+
+from custom_shell.custom_shell import CustomShell
+
+
+def _lba_to_sample_val(lba: int) -> int:
+    return lba << 4
+
+
+def _print_lba_to_sample_val(lba: int) -> None:
+    print(_lba_to_sample_val(lba))
 
 
 class TestCustomShell(TestCase):
+    def setUp(self):
+        self.__cshell = CustomShell()
+
     def test_write(self):
         pass
 
@@ -17,5 +33,11 @@ class TestCustomShell(TestCase):
     def test_full_write(self):
         pass
 
-    def test_full_read(self):
-        pass
+    @skip
+    @patch.object(CustomShell, "read", side_effect=_print_lba_to_sample_val)
+    def test_full_read(self, mk_cshell):
+        with io.StringIO() as buf, redirect_stdout(buf):
+            self.__cshell.full_read()
+            result = ', '.join(buf.getvalue().strip())
+            expected = ', '.join(str(_lba_to_sample_val(lba)) for lba in range(0, 100))
+            self.assertEqual(expected, result)
