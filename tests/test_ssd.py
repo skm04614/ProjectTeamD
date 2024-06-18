71da7a4b3c8c453c1e83e0ff535459eeb112b096
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-18 14:49:51 +0900

    [refactoring] test_ssd 중복 코드 제거

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -4,6 +4,9 @@ from ssd.ssd import SSD
 
 
 class TestSSD(TestCase):
+    def setUp(self):
+        self.ssd = SSD()
+
     def test_write(self):
         pass
 
@@ -12,22 +15,21 @@ class TestSSD(TestCase):
         expected = 0x76543210
 
         # arrange
-        sut = SSD()
-        sut.data[lba] = expected
+        self.ssd.data[lba] = expected
 
         # act
-        sut.read(lba)
+        self.ssd.read(lba)
 
         # assert
-        with open(sut.result_path, "r") as f:
+        with open(self.ssd.result_path, "r") as f:
             self.assertEqual(expected, int(f.read(), 16))
 
     @skip
     def test_read_with_invalid_lba(self):
         invalid_lbas = [-1, 100, ' ', '', None]
-        sut = SSD()
+        self.ssd = SSD()
 
         for lba in invalid_lbas:
             with self.subTest(f'SSD read test with invalid lba: {lba}'):
                 with self.assertRaises(ValueError):
-                    sut.read(lba)
+                    self.ssd.read(lba)
