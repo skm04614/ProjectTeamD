9edbe6f83b915514aef2702ce2862b17b18bb10e
ymin86kim <ymin86kim@gmail.com>
2024-06-18 14:32:15 +0900

    [test] full_write tc 업데이트

--- tests/test_custom_shell.py
+++ tests/test_custom_shell.py
@@ -1,10 +1,24 @@
+import os
 from unittest import TestCase, skip
 from unittest.mock import patch
 
-from shell import Shell
+from custom_shell.custom_shell import CustomShell
 
 
 class TestCustomShell(TestCase):
+    def setUp(self):
+        self.customShell = CustomShell()
+
+    def get_hex_values(self, file_path):
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        hex_values = []
+        for line in lines:
+            parts = line.strip().split(' ')
+            hex_value = parts[-1]
+            hex_values.append(hex_value)
+        return hex_values
+
     def test_write(self):
         pass
 
@@ -19,23 +33,19 @@ class TestCustomShell(TestCase):
 
     @skip
     def test_full_write_invalid_value(self):
-        shell = Shell()
         invalid_value = 0x1234FFFF1
         with self.assertRaises(ValueError):
-            shell.full_write(invalid_value)
+            self.customShell.full_write(invalid_value)
 
-    @skip
     def test_full_write(self):
-        shell = Shell()
-        valid_value = '0x1234FFFF'
-        file_path = './nand.txt'
-        shell.full_write(valid_value)
+        valid_value = 0x1234FFFF
+        nand_path = os.path.dirname(__file__) + "/../ssd/nand.txt"
+        self.customShell.full_write(valid_value)
 
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-        for index, line in enumerate(lines):
-            with self.subTest(f'lba:{index} value:{line.strip()}'):
-                self.assertEqual(valid_value, line.strip())
+        hex_values = self.get_hex_values(nand_path)
+        for index, line in enumerate(hex_values):
+            with self.subTest(f'lba:{index} value:{line}'):
+                self.assertEqual(valid_value, int(line))
 
     def test_full_read(self):
         pass
