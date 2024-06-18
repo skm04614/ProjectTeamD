bf866f5e0a1578c2ed419c7e427f69557896df64
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-18 14:48:09 +0900

    [feat] read with invalid lba 테스트 추가

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from ssd.ssd import SSD
 
@@ -21,3 +21,13 @@ class TestSSD(TestCase):
         # assert
         with open(sut.result_path, "r") as f:
             self.assertEqual(expected, int(f.read(), 16))
+
+    @skip
+    def test_read_with_invalid_lba(self):
+        invalid_lbas = [-1, 100, ' ', '', None]
+        sut = SSD()
+
+        for lba in invalid_lbas:
+            with self.subTest(f'SSD read test with invalid lba: {lba}'):
+                with self.assertRaises(ValueError):
+                    sut.read(lba)
