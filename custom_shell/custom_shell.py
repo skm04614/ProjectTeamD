c09224752538dce667c94c6e4af57801e462a5b5
heehoha <dolgyuk@gmail.com>
2024-06-19 14:02:46 +0900

    feat: custom_shell.py, testapp1 함수 구현 (#31)

--- custom_shell/custom_shell.py
+++ custom_shell/custom_shell.py
@@ -1,5 +1,8 @@
+import io
 import os
 import subprocess
+
+from contextlib import redirect_stdout
 
 
 class CustomShell:
@@ -63,6 +66,7 @@ class CustomShell:
         print("help()          - prints manual to stdout")
         print("full_write(val) - writes val to all lbas ranging from 0 to 99")
         print("full_read()     - reads all vals written on each lba ranging from 0 to 99 and prints to stdout")
+        print("testapp1()      - runs testapp1, which performs fullwrite and fullread")
 
         return True
 
@@ -76,6 +80,18 @@ class CustomShell:
     def full_read(self) -> bool:
         for lba in range(0, 100):
             self.read(lba)
+
+        return True
+
+    def testapp1(self) -> bool:
+        test_value = "0x1234ABCD"
+        expected_result = "\n".join([f"[{lba}] - {test_value}" for lba in range(0, 100)])
+        self.full_write(test_value)
+        with io.StringIO() as buf, redirect_stdout(buf):
+            self.full_read()
+            result = buf.getvalue().strip()
+        print(result)
+        print(f"TestApp1 was {'successful' if expected_result == result else 'failed'}!")
 
         return True
 
