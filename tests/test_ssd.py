58f6ba62ddfbd19b1f48550f5b24e0c149f41a07
Sukki (Kevin) Moon <77950368+skm04614@users.noreply.github.com>
2024-06-19 09:03:22 +0900

    feat: refactor and add new ssd unit tests (#23)

--- tests/test_ssd.py
+++ tests/test_ssd.py
@@ -1,4 +1,6 @@
-from unittest import TestCase, skip
+import os
+
+from unittest import TestCase
 
 from ssd.ssd import SSD
 
@@ -6,65 +8,80 @@ from ssd.ssd import SSD
 class TestSSD(TestCase):
     def setUp(self):
         super().setUp()
-        self.ssd = SSD()
+        self.__ssd = SSD()
 
-    @skip
-    def test_exception_when_invalid_argument_for_write(self):
-        test_arg = [[-1, 0x12345678], [101, 0x12345678], [10, 0x1234], [10, 0x1234ABCDD], [10, 'abcd']]
-
-        for lba, val in test_arg:
-            with self.assertRaises(ValueError):
-                self.ssd.write(lba, val)
-
-    @skip
-    def test_success_write(self):
-        self.ssd.write(10, 0x1234ABCD)
-        self.ssd._prepare_nand_data()
-        self.assertEqual(self.ssd._data[10], 0x1234ABCD)
-
-    @skip
-    def test_read(self):
-        lba = 33
-        expected = 0x76543210
-
-        # arrange
-        self.ssd._data[lba] = expected
-
-        # act
-        self.ssd.read(lba)
-
-        # assert
-        with open(self.ssd.result_path, "r") as f:
-            self.assertEqual(expected, int(f.read(), 16))
-
-    @skip
-    def test_read_with_out_of_range_lba(self):
-        out_of_range_lbas = [-1, 100]
-
+    def test_out_of_range_lba_write(self):
+        out_of_range_lbas = (-100, -3, -1, 100, 111, 145)
         for lba in out_of_range_lbas:
-            with self.subTest(f'SSD read test with out of range LBA: {lba}'):
-                try:
-                    self.ssd.read(lba)
-                    self.fail("ValueError should have been raised.")
-                except ValueError as err:
-                    self.assertEqual("LBA is out of range [0, 100).", err.args[0])
+            with self.assertRaises(ValueError):
+                self.__ssd.write(lba, 0x100)
 
-    @skip
-    def test_read_with_none_integer_lba(self):
-        none_integer_lbas = [' ', '', [], {}]
+    def test_mismatch_type_lba_write(self):
+        invalid_lbas = ("x", None)
+        for lba in invalid_lbas:
+            with self.assertRaises(TypeError):
+                self.__ssd.write(lba, 0x100)
 
-        for lba in none_integer_lbas:
-            with self.subTest(f'SSD read test with none integer LBA: {lba}'):
-                try:
-                    self.ssd.read(lba)
-                    self.fail("TypeError should have been raised.")
-                except TypeError as err:
-                    self.assertEqual("LBA must be an integer.", err.args[0])
+    def test_out_of_range_val_write(self):
+        out_of_range_vals = (-0x10, -0x1, 0x100000000, 0x123456789, 0xABCDABCD0)
+        for val in out_of_range_vals:
+            with self.assertRaises(ValueError):
+                self.__ssd.write(0x0, val)
 
-    @skip
-    def test_read_with_none_lba(self):
+    def test_mismatch_type_val_write(self):
+        invalid_vals = ("x", None)
+        for val in invalid_vals:
+            with self.assertRaises(TypeError):
+                self.__ssd.write(0x0, val)
+
+    def test_successful_write(self):
+        for edge_lba in (0, 99):
+            for edge_val in (0, 0xFFFFFFFF):
+                self.__ssd.write(edge_lba, edge_val)
+
+        for lba in range(0, 100, 20):
+            for val in range(0, 0x100000000, 0x10000000):
+                self.__ssd.write(lba, val)
+
+    def test_successful_write_followed_by_read(self):
+        for lba in range(0, 100, 20):
+            for val in range(0, 0x100000000, 0x20000000):
+                self.__ssd.write(lba, val)
+                self.__ssd.read(lba)
+
+                with open(self.__ssd.result_path, "r") as f:
+                    try:
+                        result = int(f.readline(), 16)
+                    except:
+                        self.fail()
+                self.assertEqual(val, result)
+
+    def test_out_of_range_lba_read(self):
+        out_of_range_lbas = (-100, -3, -1, 100, 111, 145)
+        for lba in out_of_range_lbas:
+            with self.assertRaises(ValueError):
+                self.__ssd.read(lba)
+
+    def test_mismatch_type_lba_read(self):
+        invalid_lbas = ("x", None)
+        for lba in invalid_lbas:
+            with self.assertRaises(TypeError):
+                self.__ssd.read(lba)
+
+    def test_read_new_ssd(self):
+        tmp_nand_path = "./tmp_nand.txt"
+        tmp_result_path = "./tmp_result.txt"
+        tmp_ssd = SSD(tmp_nand_path, tmp_result_path)
+
         try:
-            self.ssd.read(None)
-            self.fail("ValueError should have been raised.")
-        except ValueError as err:
-            self.assertEqual("LBA is required.", err.args[0])
+            for lba in range(0, 100):
+                tmp_ssd.read(lba)
+                with open(tmp_result_path, "r") as f:
+                    result = int(f.readline(), 16)
+                    self.assertEqual(0, result)
+        finally:
+            for filename in (tmp_result_path, tmp_nand_path):
+                try:
+                    os.remove(filename)
+                except OSError:
+                    pass
