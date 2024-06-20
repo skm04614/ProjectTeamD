e2bf036031b9e20a97bc42c0f8addd6b6c490cb8
Sunghwaa <sunghwaa.lee@gmail.com>
2024-06-20 15:03:47 +0900

    [feat] cshell 커맨드 대기 중 표시 추가

--- custom_shell/cshell.py
+++ custom_shell/cshell.py
@@ -8,7 +8,7 @@ from custom_shell.commands import *
 class CustomShell:
     def session(self) -> None:
         while True:
-            args = input().split()
+            args = input(">> ").split()
             if not args:
                 continue
 
