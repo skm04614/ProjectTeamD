4812e633672cba49c55ac8198ad5eedb4b5ae2ee
Kevin Moon <skm04614@gmail.com>
2024-06-23 21:46:28 +0900

    feat: logging added to custom_os

--- custom_operating_system/cos.py
+++ custom_operating_system/cos.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 from util import ABCSingleton
+from custom_logger import LOGGER
 
 
 class IOS(ABC, metaclass=ABCSingleton):
@@ -34,11 +35,13 @@ class CustomOS(IOS):
 
     def write_to_memory(self,
                         content: Any) -> None:
+        LOGGER.debug("")
         with (self._memory_lock,
               open(CustomOS.MEMORY_PATH, "w") as f):
             f.write(str(content))
 
     def read_from_memory(self) -> str:
+        LOGGER.debug("")
         with (self._memory_lock,
               open(CustomOS.MEMORY_PATH, "r") as f):
             return f.read().strip()
