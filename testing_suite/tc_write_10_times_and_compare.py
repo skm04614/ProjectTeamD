3496458f083239c2899f53e1f5ebdd8bb36f2617
heehoha <dolgyuk@gmail.com>
2024-06-24 15:58:37 +0900

    refactor : apply constant value

--- testing_suite/tc_write_10_times_and_compare.py
+++ testing_suite/tc_write_10_times_and_compare.py
@@ -6,7 +6,7 @@ from custom_ssd.cssd import TARGET_SSD
 def tc_write_10_times_and_compare() -> None:
     test_count = 10
     target_lbas = sample(range(TARGET_SSD.LBA_LOWER_BOUND, TARGET_SSD.LBA_UPPER_BOUND + 1), test_count)
-    target_vals = [f"0x{n:08X}" for n in sample(range(TARGET_SSD.LBA_LOWER_BOUND, 0xFFFFFFFF + 1), test_count)]
+    target_vals = [f"0x{n:08X}" for n in sample(range(TARGET_SSD.VAL_LOWER_BOUND, TARGET_SSD.VAL_UPPER_BOUND + 1), test_count)]
 
     for lba, val in zip(target_lbas, target_vals):
         command = TARGET_SSD.command_factory("W", lba, val)
