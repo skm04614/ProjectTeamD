61b670cba501b3e56b1751c186bd2c8ba93dad91
heehoha <dolgyuk@gmail.com>
2024-06-24 11:28:45 +0900

    refactor: tc_write_10_time_and_compare, tc_fullread_10_times_compare

--- testing_suite/tc_write_10_times_and_compare.py
+++ testing_suite/tc_write_10_times_and_compare.py
@@ -3,11 +3,11 @@ from random import sample, randint
 from custom_ssd.cssd import TARGET_SSD
 
 
-def tc_write_10_and_compare() -> bool:
+def tc_write_10_times_and_compare() -> bool:
+    test_count = 10
+    target_lbas = sample(range(TARGET_SSD.LBA_LOWER_BOUND, TARGET_SSD.LBA_UPPER_BOUND + 1), test_count)
+    target_vals = [f"0x{n:08X}" for n in sample(range(TARGET_SSD.LBA_LOWER_BOUND, 0xFFFFFFFF + 1), test_count)]
     try:
-        test_count = 10
-        target_lbas = sample(range(TARGET_SSD.LBA_LOWER_BOUND, TARGET_SSD.LBA_UPPER_BOUND + 1), test_count)
-        target_vals = [f"0x{n:08X}" for n in sample(range(TARGET_SSD.LBA_LOWER_BOUND, 0xFFFFFFFF + 1), test_count)]
         for lba, val in zip(target_lbas, target_vals):
             command = TARGET_SSD.command_factory("W", lba, val)
             TARGET_SSD.queue_command(command)
